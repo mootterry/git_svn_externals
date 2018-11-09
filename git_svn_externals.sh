9edcb2767e268a191dd033a6a3613a64c9fdc759
lvmaorui <lvmr@novastar.tech>
2018-11-09 11:41:14 +0800

    support other protocal(git/http/ssh) and relative url

--- git_svn_externals.sh
+++ git_svn_externals.sh
@@ -1,31 +1,94 @@
 #!/bin/bash
 PROJECT_ROOT=$PWD
-echo "It will get&update $PROJECT_ROOT"
-#sed "s?\(^.*\)\(http.*\) \(.*\)?cd $PWD\1\; [ -d \3 ] || git svn clone \"\2\" \3 || exit \$\??g" 
-#sed "s?\(^.*\)\(http.*\) \(.*\)?\1 \2 \3?g" externals.txt > tmp.txt
+VERBOSE=y
+#echo "It will get&update $PROJECT_ROOT"
 
 function git_svn_update() {
 
-	if cd "$1" && git svn rebase > /dev/null
-	then
-	 	echo "update $1 ok !!"
-	else
-	 	echo "update $1 error !!"
-	fi
+	cd "$1" || return 1
+	while true
+	do
+		if git svn rebase > /dev/null
+		then
+	 		echo "update $1 ok !!"
+	 		break
+		else
+	 		echo "update $1 error !!"
+			git st
+			echo " re|reset for git reset"
+			echo " la|late for do nothing"
+			echo " st|stash git stash"
+			echo " how you choose:"
+			read cmd
+			case "$cmd" in
+			   	re*) git reset --hard
+					;;
+			   	la*) echo "$3" >> late.log
+					break
+				;;
+			   	st*) git stash
+				;;
+			esac
+			echo "contine after <$cmd>"
+		fi
+	done
+	return 0
 }
+
 function git_svn_externals_clone() {
 	[ $# -lt 3 ] && return 0
-	#echo "=======clone $2 into $PROJECT_ROOT$1$3 ======="
+	#echo "=======clone $4$2 into $PROJECT_ROOT$1/$3 of $3======="
+	#return 0
 	cd "$PROJECT_ROOT/$1" || (echo "$1$3" error;return -1;)
 	if [ -d "$3/.git" ] 
 	then 
+		echo ">>>>>> update $1/$3 from $4$2"
 		git_svn_update "$3"
 	else
-		git svn clone "$2" "$3" 
+		echo "++++++ clone  $1/$3 from $4$2"
+		git svn clone "$4$2" "$3" 
 	fi
 }
- 
-git svn show-externals | sed 's?\(^.*\)\(http.*\) \(.*\)?\1 \2 \3?g' | while read line;
+
+function parse_args_execute() {
+	SCHEME=`echo "$1" | grep -oE '\w*://|\^/|//'`
+	case "$SCHEME" in
+		*://)
+			#echo "://"
+			git_svn_externals_clone `echo "$1" | sed 's#\(^.*\)/\(.\{2,4\}://.*\) \(.*\)#\1 \2 \3#'`
+			;;
+		^/) 
+			#echo "^^^^"
+			git_svn_externals_clone `echo "$1" | sed 's#\(^.*\)/\^\(/.*\) \(.*\)#\1 \2 \3#'` "$REPOSITORY_ROOT"
+			
+			;;
+		*) 
+			echo "**** $1"
+			;;
+	esac
+}
+
+
+REPOSITORY_ROOT=`git svn info 2>&1 | grep  "Repository Root: " | sed "s#Repository Root: \(.*\)#\1#"`
+if [ -z "$REPOSITORY_ROOT" ]
+then
+	echo "It's not a git on svn, please check it"
+	exit 1
+fi
+
+I=0
+while read line;
 do 
-	git_svn_externals_clone $line;
+	#echo "$I ==> $line"
+	AS[$I]="$line"
+	let I++
+done < <(git svn show-externals| grep "^/")
+
+#echo "TOTAL $I ==> ${AS[*]}"
+i=0
+while [ $i -lt $I ]
+do
+	#echo "$i = ${AS[$i]}"
+	let i++
+	parse_args_execute "${AS[$i]}"
 done
