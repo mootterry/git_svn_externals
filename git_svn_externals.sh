4c6cacec4c80c81f2251dd1ef5461cc50e7368ba
lvmaorui <lvmr@novastar.tech>
2018-11-13 18:27:13 +0800

    it will be stable, add svn-revision support

--- git_svn_externals.sh
+++ git_svn_externals.sh
@@ -2,52 +2,74 @@
 PROJECT_ROOT=$PWD
 VERBOSE=y
 #echo "It will get&update $PROJECT_ROOT"
-
-function git_svn_update() {
-
-	cd "$1" || return 1
-	while true
-	do
-		if git svn rebase > /dev/null
-		then
-	 		echo "update $1 ok !!"
-	 		break
-		else
-	 		echo "update $1 error !!"
-			git st
-			echo " re|reset for git reset"
-			echo " la|late for do nothing"
-			echo " st|stash git stash"
-			echo " how you choose:"
-			read cmd
-			case "$cmd" in
-			   	re*) git reset --hard
-					;;
-			   	la*) echo "$3" >> late.log
-					break
-				;;
-			   	st*) git stash
-				;;
-			esac
-			echo "contine after <$cmd>"
-		fi
-	done
-	return 0
+function message(){
+	[ "$VERBOSE" = y ] && echo $*
 }
+
+
 
 function git_svn_externals_clone() {
 	[ $# -lt 3 ] && return 0
 	#echo "=======clone $4$2 into $PROJECT_ROOT$1/$3 of $3======="
 	#return 0
-	cd "$PROJECT_ROOT/$1" || (echo "$1$3" error;return -1;)
-	if [ -d "$3/.git" ] 
-	then 
-		echo ">>>>>> update $1/$3 from $4$2"
-		git_svn_update "$3"
-	else
-		echo "++++++ clone  $1/$3 from $4$2"
-		git svn clone "$4$2" "$3" 
+	SUB_REPO_URL="$4$2"
+	SUB_REPO_NAME="$3"
+	SUB_REPO_REL_PATH="$1/$3"
+	SUB_REPO_ABS_ROOT="$PROJECT_ROOT$1"
+	SUB_REPO_CTRL=""
+	if echo "$SUB_REPO_URL" | grep "@" 
+	then
+		 SUB_REPO_REV="${SUB_REPO_URL##*@}"
+		 SUB_REPO_URL="${SUB_REPO_URL%%@*}"
+		 SUB_REPO_CTRL="-r $SUB_REPO_REV"
+		 echo ">>>>>> REVISON  SUB_REPO_URL=$SUB_REPO_URL SUB_REPO_REV=$SUB_REPO_REV SUB_REPO_CTRL=$SUB_REPO_CTRL"
 	fi
+
+	[ -d "$SUB_REPO_ABS_ROOT" ] || (echo "$1$3" error;return -1;)
+	while true
+	do
+		if [ -d "$SUB_REPO_ABS_ROOT/$SUB_REPO_NAME/.git" ] 
+		then 
+			echo ">>>>>> update $SUB_REPO_ABS_ROOT/$SUB_REPO_NAME"
+			cd "$SUB_REPO_ABS_ROOT/$SUB_REPO_NAME"
+			git svn fetch $SUB_REPO_CTRL
+			if git svn rebase -l > /dev/null
+			then
+		 		echo "update $SUB_REPO_REL_PATH ok !!"
+		 		break
+			else
+		 		echo '''
+	update $SUB_REPO_REL_PATH error !!
+	>>>>>====================================================>'''
+				git status
+				echo '''
+	<<<<<====================================================>
+here some suguestion:
+ cl|clean it will do "git reset --hard"
+ st|stash it will do "git stash"
+ sk|skip  it will do nothing, then you can check it at late
+ re|reinit & download, it same as "remove it clone from remote " 
+please input:'''
+				read cmd
+				case "$cmd" in
+				   	cl*) git reset --hard
+						;;
+				   	ig*) echo "$3" >> late.log
+						break
+						;;
+				   	st*) git stash
+						;;
+					re*) cd "$SUB_REPO_ABS_ROOT" && rm -rf "$SUB_REPO_NAME"
+						;;
+				esac
+				echo "contine after <$cmd>"
+			fi
+		else
+			echo "++++++ clone  $SUB_REPO_ABS_ROOT/$SUB_REPO_NAME from $SUB_REPO_URL"
+			cd "$SUB_REPO_ABS_ROOT"
+			git svn clone "$SUB_REPO_URL" "$SUB_REPO_NAME" $SUB_REPO_CTRL && break
+		fi
+	done
 }
 
 function parse_args_execute() {
@@ -91,4 +113,13 @@ do
 	#echo "$i = ${AS[$i]}"
 	let i++
 	parse_args_execute "${AS[$i]}"
+done
+
+
+set -- `getopt f:vl "$@"`
+
+while [ $# -gt 0 ]
+do
+        echo $1
+        shift
 done
